2ce35bc052526acb30a4f503913f9784aeb59470
Hande Shraddha Suresh <95674349+hande2003@users.noreply.github.com>
2023-07-12 19:46:35 +0530

    minor changes made to index.js file

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-let express = require('express');
-require('dotenv').config();
+let express = require("express");
+require("dotenv").config();
 const multer = require("multer");
-const upload = multer({ dest: './public/images' });
+const upload = multer({ dest: "./public/images" });
 
 let app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
   let filename = req.file.originalname;
   let fileType = req.file.mimetype;
   let fileSize = req.file.size;
-  res.render('result', {
+  res.render("result", {
     file: filename,
     type: fileType,
-    size: fileSize
+    size: fileSize,
   });
-})
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, function () {
+  console.log(
+    `Your app is listening on port ${port}. Go to http://localhost:${port}`
+  );
 });
